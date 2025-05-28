d93598a7bf98141860470c1a243f529aa0072458
artur <artur.wojtasik007@gmail.com>
2025-05-28 23:08:16 +0200

    role/login

---
+++ Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PetrolStationApp.Models
+{
+    public class LoginModel
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Models/Produkt.cs
@@ -0,0 +1,10 @@
+﻿namespace PetrolStationApp.Models
+{
+    public class Produkt
+    {
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+        public decimal Cena { get; set; }
+        public int Ilosc { get; set; }
+    }
+}
